3ab07da4613cd8ca1e3ac82a70a896857467ab8e
ANevgi <aryannevgi123@gmail.com>
2023-02-26 17:37:16 +0000

    added fairbalance changes to compas

--- src/compas_data.py
+++ src/compas_data.py
@@ -3,7 +3,9 @@ from typing import List, Tuple
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import make_column_selector as selector
+from sklearn.compose import ColumnTransformer
 
 class CompasData(Data):
     # NB: if ur implementation of the class takes more than one file pls put it all into sub folder
@@ -21,11 +23,14 @@ class CompasData(Data):
         """
         self.data = pd.read_csv('data/compas-scores-two-years.csv')
 
-        if preprocessing is None:
-            self.pre_processing()
+        # Do default preprocessing
+        self.pre_processing()
 
         self._X = pd.DataFrame(self.data)
         self._y = self.data['Probability'].to_numpy()
+
+        if preprocessing == "FairBalance":
+            self.FairBalance(self._X)
 
         # Create train-test split
         self._X_train, self._X_test, self._y_train, self._y_test = train_test_split(self._X, self._y,
@@ -53,6 +58,19 @@ class CompasData(Data):
 
         self.data.rename(index=str, columns={"two_year_recid": "Probability"}, inplace=True)
         self.data['Probability'] = np.where(self.data['Probability'] == 0, 1, 0)
+
+        def FairBalance(self, X):
+            numerical_columns_selector = selector(dtype_exclude=object)
+            categorical_columns_selector = selector(dtype_include=object)
+
+            numerical_columns = numerical_columns_selector(X)
+            categorical_columns = categorical_columns_selector(X)
+
+            categorical_processor = OneHotEncoder(handle_unknown = 'ignore')
+            numerical_processor = StandardScaler()
+            self.processor = ColumnTransformer([
+                ('OneHotEncoder', categorical_processor, categorical_columns),
+                ('StandardScaler', numerical_processor, numerical_columns)])
 
     def get_sensitive_column_names(self) -> List[str]:
         """
