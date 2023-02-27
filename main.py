db3e4bc7f8ac634b8cee644acf8bd32f2a1d6c1f
cmeinson <carmen.meinson.20@ucl.ac.uk>
2023-02-27 00:59:50 +0000

    Fixed metrics a bit  + running relplication

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from src import *
 
 results_filename = "test"
 
-metric_names = [Metrics.ACC]
+metric_names = Metrics.get_attribute_dependant() + Metrics.get_attribute_independant() + Metrics.get_subgroup_dependant()
 
 # All the following constants are just strings so caould also easily be read from a file or whatever. just for convenience:
 dataset = Tester.COMPAS_D
-bias_mit = Tester.FAIRMASK
-ml_method = FairMaskModel.RF
-ml_method_bias = FairMaskModel.DT
+bias_mit = Tester.FAIRBALANCE
+ml_method = FairBalanceModel.LOGR
 
 tester = Tester(os.path.join("results",results_filename))
-X, y, preds = tester.run_test(metric_names, dataset, bias_mit, ml_method, ml_method_bias)
-print(X)
+X, y, preds = tester.run_test(metric_names, dataset, bias_mit, ml_method, sensitive_attr=["sex","race"])
 print(y, preds)
