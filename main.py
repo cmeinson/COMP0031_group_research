893424ae7a4293abaa4a44b17143d40ed1f2078e
cmeinson <carmen.meinson.20@ucl.ac.uk>
2023-02-28 17:43:14 +0000

    fast interface fix

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ from src import *
 # Just an example for now
 
 repetitions = 5
-results_filename = "test"
+results_filename = "testtt"
 other = {"save_intermediate": True}
 
 datasets =  [Tester.ADULT_D, Tester.COMPAS_D]
-mls = [(Tester.BASE_ML, BaseModel.LR, None, None), 
-        (Tester.FAIRBALANCE, FairBalanceModel.LOGR, None, "FairBalance"), 
-        (Tester.BASE_ML, BaseModel.RF, None, None), 
-        (Tester.FAIRMASK, FairMaskModel.RF, FairMaskModel.DT, "FairBalance")]
+mls = [(Tester.BASE_ML, BaseModel.LR, None, "FairBalance"), 
+       (Tester.FAIRBALANCE, FairBalanceModel.LOGR, None, "FairBalance"), 
+       (Tester.BASE_ML, BaseModel.RF, None, None), 
+       (Tester.FAIRMASK, FairMaskModel.RF, FairMaskModel.DT, None)
+]
 
 metric_names = Metrics.get_all_names()
 
 # All the following constants are just strings so caould also easily be read from a file or whatever. just for convenience:
-tester = Tester(os.path.join("results",results_filename))
 
-for dataset in datasets:
-    for bias_mit, method, method2, pre in mls:
-        X, y, preds = tester.run_test(metric_names, dataset, bias_mit, method, method2, repetitions, other = other)
+for i in range(1): # diff data splits
+    tester = Tester(os.path.join("results",results_filename))
+    for dataset in datasets:
+        for bias_mit, method, method2, pre in mls:
+            X, y, preds = tester.run_test(metric_names, dataset, bias_mit, method, method2, repetitions, data_preprocessing=pre, other = other)
