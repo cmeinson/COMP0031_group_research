67bf2d7bb5c46783131ce9b3f0284c769ed36af8
andreea_ir <andreea.irina.20@ucl.ac.uk>
2023-03-14 11:36:26 +0000

    Testing more iterations

--- fairbalance_extra_models.py
+++ fairbalance_extra_models.py
@@ -8,7 +8,7 @@ import time
 Testing the performance in mitigating bias with different ML models that
 support fit(X,y,[sample_weight]) and sparse arrays
 """
-n_repetitions = 5
+n_repetitions = [1, 5, 10, 20]
 same_data_split = True
 results_filename = "fairbalance_diff_models"
 other = {Tester.OPT_SAVE_INTERMID: False}
@@ -25,34 +25,21 @@ models_runtimes_compas = []
 
 if __name__ == "__main__":
     tester = Tester(results_file)
-    for dataset in datasets:
-        if dataset == Tester.ADULT_D:
-            for model in models:
-                start = time.time()
-                tester.run_test(metric_names, dataset, Tester.FAIRBALANCE, model, None, n_repetitions, same_data_split, data_preprocessing="FairBalance", other = other_fb)
-                end = time.time()
-                models_runtimes_adult.append((end-start)/n_repetitions)
-        elif dataset == Tester.COMPAS_D:
-            for model in models:
-                start = time.time()
-                tester.run_test(metric_names, dataset, Tester.FAIRBALANCE, model, None, n_repetitions, same_data_split, data_preprocessing="FairBalance", other = other_fb)
-                end = time.time()
-                models_runtimes_compas.append((end-start)/n_repetitions)
+    for n in n_repetitions:
+        print("No. of repetitions: ", n)
+        for dataset in datasets:
+            if dataset == Tester.ADULT_D:
+                for model in models:
+                    start = time.time()
+                    tester.run_test(metric_names, dataset, Tester.FAIRBALANCE, model, None, n, same_data_split, data_preprocessing="FairBalance", other = other_fb)
+                    end = time.time()
+                    models_runtimes_adult.append((end-start)/n)
+            elif dataset == Tester.COMPAS_D:
+                for model in models:
+                    start = time.time()
+                    tester.run_test(metric_names, dataset, Tester.FAIRBALANCE, model, None, n, same_data_split, data_preprocessing="FairBalance", other = other_fb)
+                    end = time.time()
+                    models_runtimes_compas.append((end-start)/n)
+        print("Adult runtimes: ", models_runtimes_adult)
+        print("Compas runtimes: ", models_runtimes_compas)
 
-print("Adult: ", models_runtimes_adult)
-fig, ax = plt.subplots()
-ax.bar(models, models_runtimes_adult)
-ax.set_xlabel('Model')
-ax.set_ylabel('Avg of running time')
-ax.set_title('Avg of running times of different ML models on Adult dataset')
-plt.legend(['Time (sec)'], loc='upper right')
-plt.show()
-
-print("Compas: ", models_runtimes_compas)
-fig, ax = plt.subplots()
-ax.bar(models, models_runtimes_compas)
-ax.set_xlabel('Model')
-ax.set_ylabel('Avg of running time')
-ax.set_title('Avg of running times of different ML models on Compas dataset')
-plt.legend(['Time (sec)'], loc='upper right')
-plt.show()
