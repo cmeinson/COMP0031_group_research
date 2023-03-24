945e96bd277c0d5b2443d2bc1c74842fa3c19a7e
andreea_ir <andreea.irina.20@ucl.ac.uk>
2023-03-24 10:13:26 +0000

    Run both methods with FairBalance preprocessing

--- baseline.py
+++ baseline.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-n_repetitions = 50
+n_repetitions = 1
 same_data_split = True
 results_filename = "fairbalance_fairmask"
 other = {Tester.OPT_SAVE_INTERMID: False}
@@ -13,11 +13,20 @@ other_fb = other.copy()
 other_fb[BaseModel.OPT_FBALANCE] = True
 
 datasets =  [Tester.ADULT_D, Tester.COMPAS_D]
-mls = [(Tester.FAIRMASK, Model.RF_C, Model.DT_R, None, other), 
+#FairMask preprocessing
+mls1 = [(Tester.FAIRMASK, Model.RF_C, Model.DT_R, None, other_fb), 
        (Tester.FAIRBALANCE, Model.LG_R, None, None, other_fb), 
        (Tester.REWEIGHING, Model.LG_R, None, None, other_fb), 
        (Tester.BASE_ML, Model.LG_R, None, None, other_fb), 
 ]
+
+#FairBalance preprocessing
+mls2 = [(Tester.FAIRMASK, Model.RF_C, Model.RF_C, "FairBalance", other_fb), 
+       (Tester.FAIRBALANCE, Model.LG_R, None, "FairBalance", other_fb), 
+       (Tester.REWEIGHING, Model.LG_R, None, "FairBalance", other_fb), 
+       (Tester.BASE_ML, Model.LG_R, None, "FairBalance", other_fb), 
+]
+
 metric_names = [Metrics.ACC, Metrics.F1, Metrics.PRE, Metrics.REC ,Metrics.M_EOD, Metrics.M_AOD, Metrics.DF, Metrics.SF]
 results_file = os.path.join("results",results_filename +".csv")
 
@@ -25,6 +34,6 @@ results_file = os.path.join("results",results_filename +".csv")
 if __name__ == "__main__":
     tester = Tester(results_file)
     for dataset in datasets:
-        for bias_mit, method, method2, pre, oth in mls:
+        for bias_mit, method, method2, pre, oth in mls2:
             tester.run_test(metric_names, dataset, bias_mit, method, method2, n_repetitions, same_data_split, data_preprocessing=pre, other = oth)
 
--- src/fairmask.py
+++ src/fairmask.py
@@ -36,8 +36,11 @@ class FairMaskModel(Model):
         self._method_bias = method_bias
         self._mask_models = {}
         self._sensitive = sensitive_attributes
-
+        self.transformer = self._get_transformer(X)
+    
         X_non_sens = X.copy().drop(columns=self._sensitive)
+        self.transformer_non_sens = self._get_transformer(X_non_sens)
+        X_non_sens = self.transformer_non_sens.fit_transform(X_non_sens)
 
         # Build the mask_model for predicting each protected attribute
         for attr in sensitive_attributes: # ngl this code very sketchy but whatever will just copy what they did for now 
@@ -55,7 +58,7 @@ class FairMaskModel(Model):
             
         # Build the model for the actual prediction
         self._model = self._get_model(method)
-        self._model.fit(X, y)
+        self._model.fit(self.transformer.fit_transform(X), y)
              
 
     def predict(self, X: pd.DataFrame, other: Dict[str, Any] = {}) -> np.array:
@@ -70,13 +73,13 @@ class FairMaskModel(Model):
         :rtype: np.array
         """
         X_masked = self._mask(X)
-        return self._model.predict(X_masked)
+        return self._model.predict(self.transformer.transform(X_masked))
         
     def _mask(self, X: pd.DataFrame):
         threshold = 0.5
 
         X_out = X.copy()
-        X_non_sens = X.copy().drop(columns=self._sensitive)
+        X_non_sens = self.transformer_non_sens.transform(X.copy().drop(columns=self._sensitive))
 
         for attr in self._sensitive: 
             mask_model = self._mask_models[attr]
@@ -85,7 +88,6 @@ class FairMaskModel(Model):
                 mask = np.where(mask >= threshold, 1, 0) # substitute to the reg2clf function
             X_out[attr] = mask
 
-    
         return X_out
 
 
