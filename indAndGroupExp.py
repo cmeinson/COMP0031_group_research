079910ab2fc9f65ecca8cce52fa7431ca37fc251
aaishaniraula <aaisha.niraula.20@ucl.ac.uk>
2023-03-24 10:01:26 +0000

    rq1 done

--- indAndGroupExp.py
+++ indAndGroupExp.py
@@ -5,20 +5,21 @@ from src import *
 
 n_repetitions = 50
 same_data_split = True
-results_filename = "heheh"
+results_filename = "indAndGroup"
 other = {Tester.OPT_SAVE_INTERMID: False}
 
 other_fb = other.copy()
 other_fb[BaseModel.OPT_FBALANCE] = True
 
-datasets =  [Tester.ADULT_D, Tester.COMPAS_D, Tester.MEPS_D]#, Tester.GERMAN_D]
+datasets =  [Tester.ADULT_D, Tester.COMPAS_D, Tester.MEPS_D]
 mls = [(Tester.BASE_ML, Model.LG_R, None, "FairBalance", other_fb), 
        (Tester.FAIRBALANCE, Model.LG_R, None, "FairBalance", other_fb), 
 
        (Tester.BASE_ML, Model.RF_C, None, None, other), 
        (Tester.FAIRMASK, Model.RF_C, Model.DT_R, None, other)
 ]
-metric_names = Metrics.get_all_names()
+sensitive_attr = ['race', 'sex']
+metric_names_single = [Metrics.ACC, Metrics.PRE, Metrics.REC, Metrics.F1, Metrics.AOD, Metrics.EOD, Metrics.SPD, Metrics.DI, Metrics.FR]
 results_file = os.path.join("results",results_filename +".csv")
 
 
@@ -26,4 +27,6 @@ if __name__ == "__main__":
     tester = Tester(results_file)
     for dataset in datasets:
         for bias_mit, method, method2, pre, oth in mls:
-            tester.run_test(metric_names, dataset, bias_mit, method, method2, n_repetitions, same_data_split, data_preprocessing=pre, other = oth)
+            # Passing 1 sensitive attribute
+            for attr in sensitive_attr:
+                tester.run_test(metric_names_single, dataset, bias_mit, method, method2, n_repetitions, same_data_split, data_preprocessing=pre, sensitive_attr = [attr], other = oth)
--- subgroupExp.py
+++ subgroupExp.py
@@ -5,20 +5,21 @@ from src import *
 
 n_repetitions = 50
 same_data_split = True
-results_filename = "heheh"
+results_filename = "subgroup"
 other = {Tester.OPT_SAVE_INTERMID: False}
 
 other_fb = other.copy()
 other_fb[BaseModel.OPT_FBALANCE] = True
 
-datasets =  [Tester.ADULT_D, Tester.COMPAS_D, Tester.MEPS_D]#, Tester.GERMAN_D]
+datasets =  [Tester.ADULT_D, Tester.COMPAS_D]
 mls = [(Tester.BASE_ML, Model.LG_R, None, "FairBalance", other_fb), 
        (Tester.FAIRBALANCE, Model.LG_R, None, "FairBalance", other_fb), 
 
        (Tester.BASE_ML, Model.RF_C, None, None, other), 
        (Tester.FAIRMASK, Model.RF_C, Model.DT_R, None, other)
 ]
-metric_names = Metrics.get_all_names()
+sensitive_attr = ['race', 'sex']
+metric_names_multiple = [Metrics.ACC, Metrics.PRE, Metrics.REC, Metrics.F1, Metrics.M_AOD, Metrics.M_EOD, Metrics.SF, Metrics.DF]
 results_file = os.path.join("results",results_filename +".csv")
 
 
@@ -26,4 +27,5 @@ if __name__ == "__main__":
     tester = Tester(results_file)
     for dataset in datasets:
         for bias_mit, method, method2, pre, oth in mls:
-            tester.run_test(metric_names, dataset, bias_mit, method, method2, n_repetitions, same_data_split, data_preprocessing=pre, other = oth)
+            #Passing 2 sensitive attributes
+            tester.run_test(metric_names_multiple, dataset, bias_mit, method, method2, n_repetitions, same_data_split, data_preprocessing=pre, other = oth)
